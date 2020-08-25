543a125e5cfcccc5aee31ef9bd168da0f2fd73f7
Kat Lee <kalee@student.neumont.edu>
2020-08-24 18:38:12 -0600

    added body-parser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var bodyParser= require('body-parser')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const InitiateMongoServer = require('./config/db');
@@ -30,9 +31,12 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
