e1ce0d2d5a70e4e712bffa385e721120b3378ac2
Kat Lee <kalee@student.neumont.edu>
2020-08-30 02:04:23 -0600

    had to comment out some code

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -2,11 +2,11 @@ function dropDown() {
     document.getElementById("myDropdown").classList.toggle("show")
   }
 
-window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-        let myDropdown = document.getElementById("myDropdown")
-        if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show')
-      }
-    }
-}
+//window.onclick = function(e) {
+  //  if (!e.target.matches('.dropbtn')) {
+    //    let myDropdown = document.getElementById("myDropdown")
+      //  if (myDropdown.classList.contains('show')) {
+        //myDropdown.classList.remove('show')
+      //}
+   // }
+//}
