e9252b9f6f0e9d23fc652297c08cd818a0c4c27c
Kat Lee <kalee@student.neumont.edu>
2020-08-30 01:59:07 -0600

    added createProduct.js

---
+++ public/javascripts/createProduct.js
@@ -0,0 +1,34 @@
+const newProduct = document.getElementById('productCreateButton');
+newProduct.addEventListener('click', function(e) {
+  console.log('test');
+  e.preventDefault();
+
+  fetch('/createProduct', {method: 'POST'})
+    .then(function(response) {
+      if(response.ok) {
+        console.log('Product was recorded');
+        return;
+      }
+      throw new Error('Request failed.');
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
